e4edbb072542ee0a2d85289f3196d880e1502306
Arif870 <arifuzzaman959@gmail.com>
2021-11-13 02:24:17 +0600

    all products data loaded

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.td49h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -19,6 +20,7 @@ async function run() {
     await client.connect();
     const database = client.db("auqadb");
     const productsCollection = database.collection("allproducts");
+    const purchaseCollection = database.collection("purchase_collection");
 
     app.post("/addnewproduct", async (req, res) => {
       const addProduct = req.body;
@@ -29,6 +31,25 @@ async function run() {
     app.get("/allproducts", async (req, res) => {
       const cursor = await productsCollection.find({});
       const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    app.get("/allproducts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const addProduct = await productsCollection.findOne(query);
+      res.json(addProduct);
+    });
+
+    app.get("/allorders", async (req, res) => {
+      const cursor = await purchaseCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    app.post("/completePurches", async (req, res) => {
+      const completePurchase = req.body;
+      const result = await purchaseCollection.insertOne(completePurchase);
       res.json(result);
     });
   } finally {
